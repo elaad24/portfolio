a4acef410983d4c323d31d01c3c2d81eb6a8a875
unknown <elaad2408@gmail.com>
2021-10-16 23:59:59 +0300

    created mini segment for education  info

---
+++ src/components/Education.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div>
+      <h3 className="mini-title ">EDUCATION</h3>
+
+      <div className="small-Text">
+        <p>
+          <b>Aug 2020 - Apr 2021</b>
+          <br />
+          &#8226; Full Stack Development- Hackeru college.
+          <br />
+          &#9866; GPA: 100
+          <br />
+          <br />
+          <b>Sep 2013 - Jun 2016</b>
+          <br />
+          &#8226; High School Diploma - Gimnasia Realit
+        </p>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Education;
