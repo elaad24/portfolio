96d9c40d647896980a6c75d71ade068c866ac979
unknown <elaad2408@gmail.com>
2021-10-16 23:59:39 +0300

    created mini segment for militery info

---
+++ src/components/Army.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Army = () => {
+  return (
+    <div>
+      <h3 className="mini-title text-center">ARMY</h3>
+      <div className="small-Text">
+        <div>
+          <b>March 2017 - November 2019</b>
+          <br />
+          <p className="intro ">
+            Warrior in the Combat Engineering Corps, released with the rank of
+            First Sergeant.
+          </p>
+          <ul>
+            <li>
+              Comprehend and analyze information in short periods of time.
+            </li>
+            <li>
+              Experience in writing lesson plans and delivering them, managing
+              feedback and standing in front of an audience.
+            </li>
+            <li>
+              Working with a variety of officials - from battalion commander to
+              simple soldiers.
+            </li>
+            <li>
+              Immediate preparation for the transition from routine to
+              emergency.
+            </li>
+            <li>
+              Filling in for the intelligence officer in his absence, various
+              training sessions for soldiers work in short periods of time and
+              working with various interfaces
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Army;
