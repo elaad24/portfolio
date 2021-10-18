178c8323de891db7ceb555fb4c88a3342148ff66
unknown <elaad2408@gmail.com>
2021-10-19 00:42:30 +0300

    added and fix the carousel data base

---
+++ src/carousel_DB.js
@@ -0,0 +1,351 @@
+import e_commerce1 from "./images/e_commerce/1.png";
+import e_commerce2 from "./images/e_commerce/2.png";
+import e_commerce3 from "./images/e_commerce/3.png";
+import e_commerce4 from "./images/e_commerce/4.png";
+import e_commerce5 from "./images/e_commerce/5.png";
+import cash_register2 from "./images/cash_register/2.png";
+import cash_register1 from "./images/cash_register/1.png";
+import cash_register3 from "./images/cash_register/3.png";
+import cash_register4 from "./images/cash_register/4.png";
+import cash_register6 from "./images/cash_register/6.png";
+import cash_register5 from "./images/cash_register/5.png";
+import cash_register7 from "./images/cash_register/7.png";
+import cash_register9 from "./images/cash_register/9.png";
+import cash_register8 from "./images/cash_register/8.png";
+import interview_project1 from "./images/interview_project/1.png";
+import interview_project2 from "./images/interview_project/2.png";
+import interview_project3 from "./images/interview_project/3.png";
+import interview_project4 from "./images/interview_project/4.png";
+import interview_project5 from "./images/interview_project/5.png";
+import sms1 from "./images/sms/1.png";
+import sms2 from "./images/sms/2.png";
+import sms3 from "./images/sms/3.png";
+import sms4 from "./images/sms/4.png";
+import sms5 from "./images/sms/5.png";
+import sms6 from "./images/sms/6.png";
+import mardi_gra1 from "./images/mardi_gra/1.png";
+import mardi_gra2 from "./images/mardi_gra/2.png";
+import mardi_gra3 from "./images/mardi_gra/3.png";
+import mardi_gra4 from "./images/mardi_gra/4.png";
+import mardi_gra5 from "./images/mardi_gra/5.png";
+import mardi_gra6 from "./images/mardi_gra/6.png";
+import mardi_gra8 from "./images/mardi_gra/8.png";
+import mardi_gra9 from "./images/mardi_gra/9.png";
+import mardi_gra10 from "./images/mardi_gra/10.png";
+import mardi_gra11 from "./images/mardi_gra/11.png";
+import mardi_gra12 from "./images/mardi_gra/12.png";
+import quiz1 from "./images/quiz/1.png";
+import quiz2 from "./images/quiz/2.png";
+import quiz3 from "./images/quiz/3.png";
+import quiz4 from "./images/quiz/4.png";
+import quiz6 from "./images/quiz/6.png";
+import crypto_front1 from "./images/crypto_front/1.png";
+import crypto_front2 from "./images/crypto_front/2.png";
+import milk_and_honey1 from "./images/milk_and_honey/1.png";
+import milk_and_honey2 from "./images/milk_and_honey/2.png";
+import milk_and_honey3 from "./images/milk_and_honey/3.png";
+import milk_and_honey4 from "./images/milk_and_honey/4.png";
+import milk_and_honey5 from "./images/milk_and_honey/5.png";
+import hang_man3 from "./images/hang_man/3.png";
+import hang_man2 from "./images/hang_man/2.png";
+import hang_man1 from "./images/hang_man/1.png";
+import hang_man4 from "./images/hang_man/4.png";
+import api1 from "./images/api/1.png";
+import api2 from "./images/api/2.png";
+import api3 from "./images/api/3.png";
+
+export default {
+  projects: [
+    {
+      name: "e_commerce",
+      type: "full stack",
+      technology: "react,redux,node.js,express.js,mongo-DB,responsive",
+      images: [
+        {
+          title: "product page",
+          image: e_commerce1,
+        },
+        {
+          title: "add new product page",
+          image: e_commerce2,
+        },
+        {
+          title: "shoppng cart page",
+          image: e_commerce3,
+        },
+        {
+          title: "home page",
+          image: e_commerce4,
+        },
+        {
+          title: "sing-in page",
+          image: e_commerce5,
+        },
+      ],
+    },
+    {
+      name: "cash register",
+      type: "full stack",
+      technology: "react,redux,next.js,Mysql",
+      images: [
+        {
+          title: "store product interface",
+          image: cash_register2,
+        },
+        {
+          title: "navigation page",
+          image: cash_register1,
+        },
+        {
+          title: "cash payment page - unpaid",
+          image: cash_register3,
+        },
+        {
+          title: "cash payment page - paid",
+          image: cash_register4,
+        },
+        {
+          title: "credit payment page",
+          image: cash_register6,
+        },
+        {
+          title: "store product interface -after transaction",
+          image: cash_register5,
+        },
+        {
+          title: "product data page",
+          image: cash_register7,
+        },
+        {
+          title: "edit product data",
+          image: cash_register9,
+        },
+        {
+          title: "workers data page",
+          image: cash_register8,
+        },
+      ],
+    },
+    {
+      name: "interview project",
+      type: "full stack",
+      technology: "react,redux,node.js,express.js,Mysql,responsive",
+      images: [
+        {
+          title: "product page",
+          image: interview_project1,
+        },
+        {
+          title: "product + item in shopping cart ",
+          image: interview_project2,
+        },
+        {
+          title: "item seles states ",
+          image: interview_project3,
+        },
+        {
+          title: "product data page",
+          image: interview_project4,
+        },
+        {
+          title: "edit product data",
+          image: interview_project5,
+        },
+      ],
+    },
+    {
+      name: "school management system",
+      type: "full stack",
+      technology: "php,Mysql,responsive",
+      images: [
+        {
+          title: "first page",
+          image: sms1,
+        },
+        {
+          title: "login page ",
+          image: sms2,
+        },
+        {
+          title: "strong user contact page ",
+          image: sms3,
+        },
+        {
+          title: "strong user grades page",
+          image: sms4,
+        },
+        {
+          title: "strong user add grades page",
+          image: sms5,
+        },
+        {
+          title: "admin page add user",
+          image: sms6,
+        },
+      ],
+    },
+    {
+      name: "mardi_gra",
+      type: "front end ",
+      technology: "html5 , sass , vanila js , responsive ",
+      images: [
+        {
+          title: "opening video",
+          image: mardi_gra1,
+        },
+        {
+          title: "links and warning",
+          image: mardi_gra2,
+        },
+        {
+          title: "mayor letter",
+          image: mardi_gra3,
+        },
+        {
+          title: "parallax effect and animations",
+          image: mardi_gra4,
+        },
+        {
+          title: "attractions preview",
+          image: mardi_gra5,
+        },
+        {
+          title: "about page",
+          image: mardi_gra6,
+        },
+        {
+          title: "media page",
+          image: mardi_gra8,
+        },
+        {
+          title: "inside media category ",
+          image: mardi_gra9,
+        },
+        {
+          title: "store page ",
+          image: mardi_gra10,
+        },
+        {
+          title: "FAQ page ",
+          image: mardi_gra11,
+        },
+        {
+          title: "cantact us ",
+          image: mardi_gra12,
+        },
+      ],
+    },
+    {
+      name: "quiz game",
+      type: "front end ",
+      technology: "react, redux , responsive",
+      images: [
+        {
+          title: "start page",
+          image: quiz1,
+        },
+        {
+          title: "quiz interface",
+          image: quiz2,
+        },
+        {
+          title: "true answer interface",
+          image: quiz3,
+        },
+        {
+          title: "false answer interface",
+          image: quiz4,
+        },
+        {
+          title: "results page",
+          image: quiz6,
+        },
+      ],
+    },
+    {
+      name: "milk and honey - intervew",
+      type: "front end ",
+      technology: "react, redux-toolkit , responsive",
+      images: [
+        {
+          title: "landing page ",
+          image: milk_and_honey1,
+        },
+        {
+          title: "product page - by category",
+          image: milk_and_honey2,
+        },
+        {
+          title: "contact us page",
+          image: milk_and_honey3,
+        },
+        {
+          title: "shopping cart page ",
+          image: milk_and_honey4,
+        },
+        {
+          title: "product page ",
+          image: milk_and_honey5,
+        },
+      ],
+    },
+    {
+      name: "crypto front - interview project",
+      type: "front end ",
+      technology: "react",
+      images: [
+        {
+          title: "crypto data page",
+          image: crypto_front1,
+        },
+        {
+          title: "crypto data - highlight",
+          image: crypto_front2,
+        },
+      ],
+    },
+
+    {
+      name: "hang man game",
+      type: "front end ",
+      technology: "react, javaScript , responsive",
+      images: [
+        {
+          title: "start game",
+          image: hang_man3,
+        },
+        {
+          title: "lose in game",
+          image: hang_man2,
+        },
+        {
+          title: "win in game",
+          image: hang_man1,
+        },
+        {
+          title: "before game",
+          image: hang_man4,
+        },
+      ],
+    },
+    {
+      name: "covid api",
+      type: "back end ",
+      technology: "node.js, express.js , mysql",
+      images: [
+        {
+          title: "get all data",
+          image: api1,
+        },
+        {
+          title: "get data by query string",
+          image: api2,
+        },
+        {
+          title: "get data by params",
+          image: api3,
+        },
+      ],
+    },
+  ],
+};
